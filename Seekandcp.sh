226fcb71d84a05af2def3007d82be1be3a21fc73
Matt <TheDraca@users.noreply.github.com>
2018-02-04 11:27:08 +0000

    File types to find now in array
    For loop added too rather than manual calling of copy everytime

---
+++ Seekandcp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SearchLocation="/media/user/mountedHDD"
+CopyLocation="/media/user/USBSTICK"
+CopyLocation="${CopyLocation}/SEEKCP"
+
+copy () {
+  echo "Will $1 into $2"
+  echo "Copying ${Folder}"
+  mkdir ${CopyLocation}/${Folder} &>/dev/null
+  find ${SearchLocation} -name "$ext" -exec cp {} ${CopyLocation}/${Folder}/ \;
+}
+
+declare -A CopyTypes=( ["*.doc*"]="Documents" ["*.ppt*"]="Powerpoints" ["*.xl*"]="Spreadsheets" ["*.pub"]="Publisher" ["*.mp3"]="Music" ["*.wma"]="Music" ["*.avi"]="Videos" ["*.mov"]="Videos" ["*.wmv"]="Videos" ["*.mkv"]="Vidoes" ["*.flv"]="Videos" ["*.mp4"]="Videos" ["*.png"]="Images" ["*.jpeg"]=Images [*.gif]=Gifs ["*.pdf"]="PDFs")
+
+for ext in "${!CopyTypes[@]}"
+do
+  copy $ext ${CopyTypes[$ext]}
+done
